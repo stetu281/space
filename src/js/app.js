4d14811c00010c2a27eb56f459077e7145b140c7
Stefan Turner <turner.stefan281@gmail.com>
2022-10-21 14:59:54 +0200

    header styling finished

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,25 @@ document.querySelector(".header__hamburger").addEventListener("click", (e) => {
     e.target.children[0].src = hamburgerIcon;
   }
 });
+
+window.onload = () => {
+  let navItems = document.querySelectorAll(".navitem");
+  navItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+  const page = document.body.id;
+  switch (page) {
+    case "home":
+      document.querySelector(".home").classList.add("active");
+      break;
+    case "crew":
+      document.querySelector(".crew").classList.add("active");
+      break;
+    case "destination":
+      document.querySelector(".destination").classList.add("active");
+      break;
+    case "technology":
+      document.querySelector(".technology").classList.add("active");
+      break;
+  }
+};
