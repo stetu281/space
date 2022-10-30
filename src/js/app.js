3ead6cd9169fd8efcd6d4ee9ce0eefce84d2d884
Stefan Turner <turner.stefan281@gmail.com>
2022-10-30 21:30:00 +0100

    fix destination page and change nav active

--- src/js/app.js
+++ src/js/app.js
@@ -9,31 +9,14 @@ hamburger();
 
 //run code for specific pages
 if (document.body.id === "destination") {
+  document.querySelector(".destination").dataset.active = "true";
   destination();
 } else if (document.body.id === "crew") {
+  document.querySelector(".crew").dataset.active = "true";
   crew();
 } else if (document.body.id === "technology") {
+  document.querySelector(".technology").dataset.active = "true";
   technology();
+} else {
+  document.querySelector(".home").dataset.active = "true";
 }
-
-window.onload = () => {
-  let navItems = document.querySelectorAll(".navitem");
-  navItems.forEach((item) => {
-    item.classList.remove("active");
-  });
-  const page = document.body.id;
-  switch (page) {
-    case "home":
-      document.querySelector(".home").classList.add("active");
-      break;
-    case "crew":
-      document.querySelector(".crew").classList.add("active");
-      break;
-    case "destination":
-      document.querySelector(".destination").classList.add("active");
-      break;
-    case "technology":
-      document.querySelector(".technology").classList.add("active");
-      break;
-  }
-};
